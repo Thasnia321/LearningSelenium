37e2043bee0e6c965afbad27f19360ad80a13474
Thasni A <thasnia321@gmail.com>
2025-01-03 18:02:03 +0530

    commit2

--- src/test/java/org/examplethasni/ex_01_SeleniumBAsics/TestSelenium01.java
+++ src/test/java/org/examplethasni/ex_01_SeleniumBAsics/TestSelenium01.java
@@ -1,4 +1,16 @@
 package org.examplethasni.ex_01_SeleniumBAsics;
 
+import io.qameta.allure.Description;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium01 {
+
+    @Test
+    @Description("OPen vwo login page and print the title")
+    public void test_Selenium01(){
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com/#/login");
+        System.out.println(driver.getTitle());
+    }
 }
